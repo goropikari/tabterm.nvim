c4823cd7c3473746d2f5bf9001600d136d70ca2b
goropikari <goropikari56@gmail.com>
2025-08-07 22:49:25 +0900

    toggle keybind

--- lua/tabterm/init.lua
+++ lua/tabterm/init.lua
@@ -2,12 +2,29 @@ local M = {
   state = 0,
 }
 
-local default_config = {}
+local default_config = {
+  keymap = {
+    toggle = '<c-t>',
+  },
+}
 
 local config = {}
 
+local function get_keymap(key)
+  local keybind = config.keymap[key] or default_config.keymap[key]
+  assert(keybind, 'Keymap not found: ' .. key)
+  return keybind
+end
+
+local function set_keymap(modes, key, cb, desc)
+  local keymap = get_keymap(key)
+  vim.keymap.set(modes, keymap, cb, { desc = desc or '' })
+end
+
 function M.setup(opts)
   config = vim.tbl_deep_extend('force', default_config, opts or {})
+
+  set_keymap({ 'n' }, 'toggle', M.toggle, 'Toggle')
 end
 
 function M.toggle()
