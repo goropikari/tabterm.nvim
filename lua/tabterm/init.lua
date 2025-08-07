e53839b36ded7cb918543547943a0586e4a342f4
goropikari <goropikari56@gmail.com>
2025-08-07 22:42:04 +0900

    toggle template

--- lua/tabterm/init.lua
+++ lua/tabterm/init.lua
@@ -1,4 +1,6 @@
-local M = {}
+local M = {
+  state = 0,
+}
 
 local default_config = {}
 
@@ -6,6 +8,34 @@ local config = {}
 
 function M.setup(opts)
   config = vim.tbl_deep_extend('force', default_config, opts or {})
+end
+
+function M.toggle()
+  if M.is_open() then
+    M.close()
+  else
+    M.open()
+  end
+end
+
+function M.is_open()
+  return M.state == 1
+end
+
+function M.open()
+  if M.is_open() then
+    return
+  end
+  print('open')
+  M.state = 1
+end
+
+function M.close()
+  if not M.is_open() then
+    return
+  end
+  print('close')
+  M.state = 0
 end
 
 return M
